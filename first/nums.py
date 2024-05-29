dab14ee88283f3538a5c5f4ec5852cd1c455d910
Mikhail <monstrillo6gigov@gmail.com>
2024-05-29 15:41:31 +0300

    add divide func in nums.

--- first/nums.py
+++ first/nums.py
@@ -9,8 +9,12 @@ def minus(a, b):
 def multy(a, b):
     return a * b
 
+def divide(a, b):
+    return a / b
+
 
 if __name__ == '__main__':
     print(plus(1, 5))
     print(minus(1, 5))
     print(multy(5, 5))
+    print(divide(6, 3))
