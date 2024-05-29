2e2bafb36ec58a50247abd302417dd26cc918c73
Mikhail <monstrillo6gigov@gmail.com>
2024-05-29 15:02:21 +0300

    add module "string", add func "multy" in module nums.py

--- first/nums.py
+++ first/nums.py
@@ -1,9 +1,16 @@
 def plus(a, b):
     return a + b
 
+
 def minus(a, b):
     return a - b
+
+
+def multy(a, b):
+    return a * b
+
 
 if __name__ == '__main__':
     print(plus(1, 5))
     print(minus(1, 5))
+    print(multy(5, 5))
---
+++ first/string.py
@@ -0,0 +1,6 @@
+def up(text: str):
+    return text.upper()
+
+
+if __name__ == '__main__':
+    print(up('maryna'))
